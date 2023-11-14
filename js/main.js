4d7fcaf2c3ef10b38cf3807a3ba155fef51031e2
seohyon-cho <flanique@naver.com>
2023-11-14 10:15:56 +0900

    탭 메뉴 styling

--- js/main.js
+++ js/main.js
@@ -1,38 +1,9 @@
-/*
-const h1 = document.querySelector('h1');
-const txt = h1.innerText;
-let tags = '';
-
-for (let letter of txt) {
-	tags += `<span>${letter}</span>`;
-}
-
-console.log(tags);
-// HTML상에서 h1 안쪽에 기존에 있던 내용은 없애고, tags 내용으로 덮어쓰는 것.
-h1.innerHTML = tags;
-*/
-splitLetter('h1');
-splitLetter('h2');
-
-//
 function splitLetter(el) {
 	// 함수 안에 있는 지역변수는 함수 호출이 끝나고 나면 지워져 불필요하게 메모리 차지도 안 하기 때문에 성능면에서 효율적임.
 	const dom = document.querySelector(el);
 	let tags = '';
-
 	for (let letter of dom.innerText) {
 		tags += `<span>${letter}</span>`;
 	}
-
 	dom.innerHTML = tags;
 }
-
-/*
-// 내가 짠 방식 (구현 안 됨. 오류 발생)
-function splitLetter(el) {
-	txt.forEach((el) => {
-		tags += `<span>${el}</span>`;
-		h1.innerHTML = tags;
-	});
-}
-*/
